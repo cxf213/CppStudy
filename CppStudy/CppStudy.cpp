153950fa537b1cea27910cc11a6990d8b8e721cb
cxf213 <cxf213@outlook.com>
2020-10-20 16:47:59 +0800

    结构体

--- CppStudy/CppStudy.cpp
+++ CppStudy/CppStudy.cpp
@@ -4,14 +4,57 @@
 #include <iostream>
 #include <string.h>
 #include <string>
+
 using namespace std;
+
+typedef struct Node {
+    string content = "";
+    struct Node* nextnode = NULL;
+}Node;
+
+void chaim(){
+    Node node1;
+}
+
+#pragma region 位域
+
+
+
+struct Bool {
+    unsigned int a : 1; //位域
+}bol;
+
+struct Book //结构体
+{
+    string name = "default";
+    int id = 0;
+    struct Book *nextbook = NULL;
+}book = {"default",0};
+
+void structEXP() {
+    bol.a = 0;
+    cout << bol.a << endl; //调用位域
+
+    struct Book book1; //产生了结构体
+    struct Book book2;
+    book1.name = "b1";
+    book1.id = 1001;
+    book2.name = "b2";
+    book2.id = 1002;
+
+    book1.nextbook = &book2;
+
+    cout << book1.nextbook->name <<endl;
+
+}
+#pragma endregion
 
 void pointer() {
     int i = 0;
     int *p1=NULL;
     p1 = &i;
 
-    cout << *p1;
+    cout << *p1 << endl;
 
 }
 
@@ -19,19 +62,9 @@ void pointer() {
 int main()
 {
     pointer();
+    structEXP();
+
 
     cout << endl;
 }
 
-
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
