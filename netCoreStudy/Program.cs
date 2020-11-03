c2bc27c1e5ce63d1a04cb6c0c339f6f789ddb3fa
cxf213 <cxf213@outlook.com>
2020-11-03 11:16:42 +0800

    netCore!

---
+++ netCoreStudy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace netCoreStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
