203792825fa545ef75ce35f30b207deec83c4e26
cxf213 <cxf213@outlook.com>
2020-11-07 21:26:43 +0800

    作业6

--- Homework/Homework.cpp
+++ Homework/Homework.cpp
@@ -1,25 +1,16 @@
 ﻿#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+
 
 int main()
 {
-    int x, y;
-    scanf_s("%d%d", &x, &y);
-    if (x == 0 && y==0) {
-        printf("原点");
-
+    double a = 2, b = 1, ans = 0;;
+    for (int i = 0; i < 20; i++) {
+        ans += a / b;
+        a = a + b;
+        b = a - b;
     }
-    else if (x == 0 && y != 0) {
-        printf("Y轴");
-    }
-    else if (x != 0 && y == 0) {
-        printf("X轴");
-    }
-
-    if (x > 0) {
-        printf("%s", y > 0 ? "第一象限" : "第四象限");
-    }
-    else {
-        printf("%s", y > 0 ? "第二象限" : "第三象限");
-    }
+    printf("%lf", ans);
 }
-
