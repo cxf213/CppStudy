e143d4ae6aca7deef6c1b7668d9bd6acc28a81d6
cxf213 <cxf213@outlook.com>
2020-11-13 21:19:38 +0800

    自带数据结构

--- netCoreStudy/Program.cs
+++ netCoreStudy/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 
@@ -13,17 +14,33 @@ namespace netCoreStudy
 
 
             Compar<int>.Max(12,65);
+            datas();
 
 
             sw.Stop();
             TimeSpan ts = sw.Elapsed;
+            Console.WriteLine();
             Console.WriteLine("Use Time: {0} ms", ts.TotalMilliseconds);
+        }
+
+        static void datas()
+        {
+            int i = 0;
+            List<int> list1 = new List<int>() { 1,2 };
+            Stack<int> stack1 = new Stack<int>();
+            Queue<int> queue1 = new Queue<int>();
+            HashSet<int> hash1 = new HashSet<int>();
+            Dictionary<int, int> dict1 = new Dictionary<int, int>();
+            _ = dict1[1];
+            
+            i=list1[1];
+            Console.WriteLine(i);
         }
     }
 
     internal class Compar<T> where T : IComparable
     {
-        static public T Max(T a, T b) => a.CompareTo(b) > 0 ? a : b;
+        public static T Max(T a, T b) => a.CompareTo(b) > 0 ? a : b;
     }
 
 
